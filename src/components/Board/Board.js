3bf9caf2d361fb5885d2f22ee2dad10be08f005e
niravpatel129 <niravpatelp129@gmail.com>
2019-11-19 17:20:09 -0500

    win logic added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -7,17 +7,69 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      isNext: true,
+      winner: null
     };
   }
 
+  handleClick(i) {
+    // slice is better because it dosen't add a reference but makes a copy
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.isNext ? "X" : "O";
+    if (!this.calculateWinner(this.state.squares)) {
+      this.state.squares[i] ||
+        this.setState({
+          squares: squares,
+          isNext: !this.state.isNext
+        });
+    }
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const a = lines[i][0];
+      const b = lines[i][1];
+      const c = lines[i][2];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
-
+    const winner = this.calculateWinner(this.state.squares);
+    let status = "Next player: " + (this.state.isNext ? "X" : "O");
+    if (winner) {
+      status = "Winner: " + winner + " - Refresh to play again";
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div>
         <div className="status">{status}</div>
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -2,21 +2,11 @@ import React, { Component } from "react";
 import "./Square.scss";
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: null };
-  }
-
-  handleOnClick = () => {
-    const squares = this.state.squares.slice();
-    this.setState({ value: "X" });
-  };
-
   render() {
     return (
       <div className="squareComponent">
-        <button className="square" onClick={this.handleOnClick}>
-          {this.state.value}
+        <button className="square" onClick={this.props.onClick}>
+          {this.props.value}
         </button>
       </div>
     );
